2344227cf5fead785b825f4363b3035070a8647f
Rafael Santos <rafaelnsantos1@gmail.com>
2016-07-26 03:17:44 -0300

    botão funcionando

--- Assets/Scripts/carregar.cs
+++ Assets/Scripts/carregar.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 public class carregar : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	public void loadLevel()
 	{
 		Application.LoadLevel("Level");
